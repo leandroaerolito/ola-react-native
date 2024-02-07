cefb4675e7fd944c97d6a4c90039a9782f1f5c06
Leandro Rocha <leandro.aerolito@gmail.com>
2024-02-07 09:25:47 -0300

    StatusBar

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Olá Reacionários! 😎</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <View>
+        <Text>Olá React Native!</Text>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

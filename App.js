4510d4a647d795639030d70e7ab21e5a2b134f26
Leandro Rocha <leandro.aerolito@gmail.com>
2024-02-07 10:29:58 -0300

    alert-safeareaview

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const vai = () => {
+    Alert.alert("Título da janela", "Fala, Chapa!");
+  };
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <View>
-        <Text>Olá React Native!</Text>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Text>Topo/Cabeçalho</Text>
+        </View>
+        <View>
+          <Text>Conteúdo...</Text>
+        </View>
+        <View>
+          <Text>Rodapé...</Text>
+          <Button title="clique aqui" onPress={vai} />
+        </View>
+      </SafeAreaView>
     </>
   );
 }
